16e352ef5ad51ba0c3ca717a4892e0e03232b608
air-wolf007 <93246575+air-wolf007@users.noreply.github.com>
2021-10-27 19:19:12 +0900

    Update ToKanaHalf.java
    1 「ヴ」の追加 ※エヴァ対応
    2 リファクタリング

--- ToKanaHalf.java
+++ ToKanaHalf.java
@@ -32,22 +32,22 @@ public class ToKanaHalf{
 			int charNum = (int) charArr[i];
 			// ァ行
 			if (charNum >= 12449 && charNum <= 12457 && charNum % 2 == 1) {
-				strBuil.append(clacByCode(charNum, 12449, 52934));
+				strBuil.append(clacByCode1(charNum, 12449, 52934));
 			// ガ行
 			} else if (charNum >= 12460 && charNum <= 12468 && charNum % 2 == 0) {
-				strBuil.append(clacByCode(charNum, 12460, 52938));
+				strBuil.append(clacByCode1(charNum, 12460, 52938));
 				strBuil.append((char) V_CO_MARK);
 			// ザ行
 			} else if (charNum >= 12470 && charNum <= 12478 && charNum % 2 == 0) {
-				strBuil.append(clacByCode(charNum, 12470, 52933));
+				strBuil.append(clacByCode1(charNum, 12470, 52933));
 				strBuil.append((char) V_CO_MARK);
 			// ダヂ
 			} else if (charNum >= 12480 && charNum <= 12482 && charNum % 2 == 0) {
-				strBuil.append(clacByCode(charNum, 12480, 52928));
+				strBuil.append(clacByCode1(charNum, 12480, 52928));
 				strBuil.append((char) V_CO_MARK);
 			// ヅデド
 			} else if (charNum >= 12485 && charNum <= 12489 && charNum % 2 == 1) {
-				strBuil.append(clacByCode(charNum, 12485, 52925));
+				strBuil.append(clacByCode1(charNum, 12485, 52925));
 				strBuil.append((char) V_CO_MARK);
 			// ッ
 			} else if (charNum == 12483) {
@@ -61,38 +61,30 @@ public class ToKanaHalf{
 			} else if (charNum >= 12495 && charNum <= 12509) {
 				// ハ行
 				if (charNum % 3 == 0) {
-					int x = (charNum - 12495) * 2 / 3;
-					int y = charNum + 52923 - x;
-					strBuil.append((char) y);
+					strBuil.append(clacByCode2(charNum, 12495, 52923));
 				// バ行
 				} else if (charNum % 3 == 1) {
-					int x = (charNum - 12496) * 2 / 3;
-					int y = charNum + 52922 - x;
-					strBuil.append((char) y);
+					strBuil.append(clacByCode2(charNum, 12496, 52922));
 					strBuil.append((char) V_CO_MARK);
 				// パ行
 				} else if (charNum % 3 == 2) {
-					int x = (charNum - 12497) * 2 / 3;
-					int y = charNum + 52921 - x;
-					strBuil.append((char) y);
+					strBuil.append(clacByCode2(charNum, 12497, 52921));
 					strBuil.append((char) HALF_SVS_MARK);
 				}
 			// マ行
 			} else if (charNum >= 12510 && charNum <= 12514) {
-				int y = charNum + 52913;
-				strBuil.append((char) y);
+				strBuil.append(clacByCode3(charNum, 52913));
 			// ヤ行
 			} else if (charNum >= 12515 && charNum <= 12520) {
 				if (charNum % 2 == 0) {
-					strBuil.append(clacByCode(charNum, 12516, 52912));
+					strBuil.append(clacByCode1(charNum, 12516, 52912));
 				// ャ行
 				} else if (charNum % 2 == 1) {
-					strBuil.append(clacByCode(charNum, 12515, 52873));
+					strBuil.append(clacByCode1(charNum, 12515, 52873));
 				}
 			// ラ行
 			} else if (charNum >= 12521 && charNum <= 12525) {
-				int y = charNum + 52910;
-				strBuil.append((char) y);
+				strBuil.append(clacByCode3(charNum, 52910));
 			// ヲン
 			} else if (charNum >= 12530 && charNum <= 12531) {
 				int x = (charNum - 12530) * 54;
@@ -100,27 +92,39 @@ public class ToKanaHalf{
 				strBuil.append((char) y);
 			// ワ
 			} else if (charNum == 12527) {
-				int y = charNum + 52909;
-				strBuil.append((char) y);
+				strBuil.append(clacByCode3(charNum, 52909));
 			// ー
 			} else if (charNum == 12540) {
-				int y = charNum + 52852;
-				strBuil.append((char) y);
+				strBuil.append(clacByCode3(charNum, 52852));
+			// ヴ
+			} else if (charNum == 12532) {
+				strBuil.append(clacByCode3(charNum, 52863));
+				strBuil.append((char) V_CO_MARK);
 			// （）＊＋，ー．／０１２３４５６７８９
 			} else if (charNum >= 65288 && charNum <= 65305) {
 				int y = charNum - 65248;
 				strBuil.append((char) y);
 			// 全般
 			} else if (charNum >= 12450 && charNum <= 12488) {
-				strBuil.append(clacByCode(charNum, 12450, 52943));
+				strBuil.append(clacByCode1(charNum, 12450, 52943));
 			}
 		}
 
 		return strBuil.toString();
 
 	}
-	private static char clacByCode(int charNum, int num1, int num2) {
+	private static char clacByCode1(int charNum, int num1, int num2) {
 		int x = (charNum - num1) / 2;		
 		return (char) (charNum + num2 - x);
 	}
+	
+	private static char clacByCode2(int charNum, int num1, int num2) {
+		int x = (charNum - num1) * 2 / 3;		
+		return (char) (charNum + num2 - x);
+	}
+	
+	private static char clacByCode3(int charNum, int num1) {
+		return (char) (charNum + num1);
+	}
+
 }
